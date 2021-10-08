b02f9e5daec3c232adc12ff61060034a3c700c4a
Tomás Ellis <tomasitoellis@gmail.com>
2021-10-08 00:44:56 -0300

    feat: fully working web page, with little user feedback

---
+++ web/pages/components/Player1UI.tsx
@@ -0,0 +1,224 @@
+import { ContractFactory, ethers } from "ethers";
+import { solidityKeccak256 } from "ethers/lib/utils";
+import Peer from "peerjs";
+import react, { useEffect, useState } from "react";
+import { RPS, RPS__factory } from "../../public/utils/typechain";
+import styles from "../../styles/Home.module.css";
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASEURL || "http://localhost:3000";
+const SALT = 1;
+
+const Player1UI = () => {
+  const [weapon, setWeapon] = useState<number>(0);
+  const [stake, setStake] = useState<string>("");
+  const [player2Address, setPlayer2Address] = useState<string>("");
+  const [matchAddress, setMatchAddress] = useState<string>("");
+  const [peerId, setPeerId] = useState<string>("");
+  const [peerState, setPeerState] = useState<Peer>();
+  const [player2Response, setPlayer2Response] = useState<boolean>(false);
+
+  enum Selection {
+    "Null",
+    "Rock",
+    "Paper",
+    "Scissors",
+    "Spock",
+    "Lizard",
+  }
+
+  const createButtons = () => {
+    let buttons = [];
+
+    for (const value in Selection) {
+      if (isNaN(Number(value)) || Selection[value] === "Null") {
+        continue;
+      }
+      buttons.push(
+        <button
+          key={value}
+          style={{ border: weapon === parseInt(value) ? "2px solid red" : "" }}
+          onClick={() => setWeapon(parseInt(value))}
+        >
+          {Selection[value]}
+        </button>
+      );
+    }
+
+    return buttons;
+  };
+
+  const createMatch = async (
+    stake: string,
+    weapon: number,
+    player2Address: string
+  ) => {
+    try {
+      // @ts-ignore
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+
+        const factory = new ContractFactory(
+          RPS__factory.abi,
+          RPS__factory.bytecode,
+          signer
+        ) as RPS__factory;
+
+        const p1Hash = solidityKeccak256(["uint8", "uint256"], [weapon, SALT]);
+
+        const RPSDeployed = await factory.deploy(p1Hash, player2Address, {
+          value: ethers.utils.parseEther(stake),
+        });
+
+        console.log("Deploying...");
+        await RPSDeployed.deployed();
+        console.log("Deployed !!", RPSDeployed.address);
+        setMatchAddress(RPSDeployed.address);
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const checkWhoWon = async () => {
+    try {
+      // @ts-ignore
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const RPSContract = new ethers.Contract(
+          matchAddress,
+          RPS__factory.abi,
+          signer
+        ) as RPS;
+
+        console.log("Checking who won");
+        const solveTx = await RPSContract.solve(weapon, SALT);
+        await solveTx.wait();
+        console.log("Done checking! Check your wallet!");
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const buttons = createButtons();
+
+  useEffect(() => {
+    import("peerjs").then(async ({ default: Peer }) => {
+      const initPeer = (): Promise<Peer> =>
+        new Promise((resolve, reject) => {
+          const peer = new Peer();
+          peer.on("error", (err) => {
+            console.error(err);
+            reject(`Could not create peer ${err.toString()}`);
+          });
+
+          peer.on("open", (_) => {
+            resolve(peer);
+          });
+        });
+      const peer = await initPeer();
+      setPeerState(peer);
+      setPeerId(peer.id);
+    });
+  }, []);
+
+  // When we get the contract address, create an event listener
+  useEffect(() => {
+    if (matchAddress !== "" && peerState) {
+      peerState.on("connection", (conn) => {
+        conn.on("data", (data) => {
+          if (data.justPlayed !== undefined) {
+            setPlayer2Response(true);
+          }
+          conn.send("We are connected, glhf");
+          console.log(data);
+          conn.send({ address: matchAddress });
+        });
+      });
+      console.log("Set contract address for sendoff");
+    }
+  }, [matchAddress]);
+
+  return (
+    <div className={styles.container}>
+      <span>
+        {peerId} -- {matchAddress}
+      </span>
+      {matchAddress === "" ? (
+        <div>
+          <div>Choose an option: {buttons}</div>
+          <br />
+
+          <div>
+            Your opponent's address:{" "}
+            <input
+              placeholder={"Please input here"}
+              onChange={(e) => {
+                setPlayer2Address(e.target.value);
+              }}
+            ></input>
+          </div>
+          <br />
+
+          <div>
+            How much do you want to stake:{" "}
+            <input
+              placeholder={"Stake here"}
+              onChange={(e) => {
+                setStake(e.target.value);
+              }}
+            ></input>
+          </div>
+          <br />
+
+          <button
+            disabled={
+              weapon !== 0 &&
+              ethers.utils.isAddress(player2Address) &&
+              parseFloat(stake) > 0 &&
+              peerId !== ""
+                ? false
+                : true
+            }
+            onClick={() => createMatch(stake, weapon, player2Address)}
+          >
+            Create match
+          </button>
+        </div>
+      ) : (
+        <div>
+          <div>
+            The match has started, share this with your opponent: {BASE_URL}
+            /?peerId={peerId}
+          </div>
+          <div>Now playing: {Selection[weapon]} vs ...</div>
+          <div>
+            {player2Response === false ? (
+              "Awaiting player 2's move"
+            ) : (
+              <button
+                onClick={() => {
+                  checkWhoWon();
+                }}
+              >
+                Check who won
+              </button>
+            )}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Player1UI;
---
+++ web/pages/components/Player2UI.tsx
@@ -0,0 +1,184 @@
+import { ContractFactory, ethers } from "ethers";
+import { solidityKeccak256 } from "ethers/lib/utils";
+import Peer from "peerjs";
+import react, { useEffect, useState } from "react";
+import { RPS, RPS__factory } from "../../public/utils/typechain";
+import styles from "../../styles/Home.module.css";
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASEURL || "http://localhost:3000";
+
+const Player2UI = ({
+  peerId,
+  currentAccount,
+}: {
+  peerId: string;
+  currentAccount: string;
+}) => {
+  const [weapon, setWeapon] = useState<number>(0);
+  const [stake, setStake] = useState<string>("");
+  const [contractAddress, setContractAddress] = useState<string>("");
+  const [waitingForPlayer, setWaitingForPlayer] = useState<boolean>(false);
+  const [connToPlayer, setConnToPlayer] = useState<Peer.DataConnection>();
+
+  enum Selection {
+    "Null",
+    "Rock",
+    "Paper",
+    "Scissors",
+    "Spock",
+    "Lizard",
+  }
+
+  const createButtons = () => {
+    let buttons = [];
+
+    for (const value in Selection) {
+      if (isNaN(Number(value)) || Selection[value] === "Null") {
+        continue;
+      }
+      buttons.push(
+        <button
+          key={value}
+          style={{ border: weapon === parseInt(value) ? "2px solid red" : "" }}
+          onClick={() => setWeapon(parseInt(value))}
+        >
+          {Selection[value]}
+        </button>
+      );
+    }
+
+    return buttons;
+  };
+
+  const buttons = createButtons();
+
+  useEffect(() => {
+    import("peerjs").then(async ({ default: Peer }) => {
+      const initPeer = (): Promise<Peer> =>
+        new Promise((resolve, reject) => {
+          const peer = new Peer();
+          peer.on("error", (err) => {
+            console.error(err);
+            reject(`Could not create peer ${err.toString()}`);
+          });
+
+          peer.on("open", (_) => {
+            resolve(peer);
+          });
+        });
+
+      const peer = await initPeer();
+
+      const conn = peer.connect(peerId, { reliable: true });
+
+      conn.on("open", () => {
+        conn.send("We are connected!");
+        // Receive messages
+        conn.on("data", (data) => {
+          console.log(data);
+          if (data.address) {
+            setContractAddress(data.address);
+          }
+        });
+      });
+      setConnToPlayer(conn);
+    });
+  }, []);
+
+  const checkContract = async () => {
+    try {
+      // @ts-ignore
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+
+        const RPSContract = new ethers.Contract(
+          contractAddress,
+          RPS__factory.abi,
+          signer
+        ) as RPS;
+        const stakeValueRaw = await RPSContract.stake();
+        const stakeValue = ethers.utils.formatEther(stakeValueRaw);
+
+        setStake(stakeValue);
+        console.log("Setting stake", stakeValue);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const sendWeaponChoice = async (
+    weapon: number,
+    contractAddress: string,
+    stake: string
+  ) => {
+    try {
+      // @ts-ignore
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+
+        const RPSContract = new ethers.Contract(
+          contractAddress,
+          RPS__factory.abi,
+          signer
+        ) as RPS;
+
+        console.log("STAKEANDOOOOOo", stake);
+
+        console.log("Sending P2 move...");
+        const playTx = await RPSContract.play(weapon, {
+          value: ethers.utils.parseEther(stake),
+        });
+        await playTx.wait();
+
+        console.log("Sent P2 move ---- !");
+        setWaitingForPlayer(true);
+        connToPlayer?.send({ justPlayed: true });
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (contractAddress !== "") {
+      checkContract();
+    }
+  }, [contractAddress]);
+  return (
+    <div className={styles.container}>
+      {waitingForPlayer === true ? (
+        "Waiting for player..."
+      ) : (
+        <div>
+          <span>{contractAddress}</span>
+          <div>Choose an option: {buttons}</div>
+          <br />
+
+          <div>You are about to stake: {stake !== "" ? stake : "..."} ETH</div>
+          <br />
+          <button
+            disabled={
+              weapon !== 0 && contractAddress !== "" && stake !== ""
+                ? false
+                : true
+            }
+            onClick={() => sendWeaponChoice(weapon, contractAddress, stake)}
+          >
+            Send move!
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Player2UI;
